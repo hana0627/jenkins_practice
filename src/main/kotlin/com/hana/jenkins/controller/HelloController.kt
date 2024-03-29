0f4820ccb37a59448134f1270d3cf3765fef79a2
hana0627 <hanana9506@naver.com>
2024-03-29 13:10:54 +0900

    commit

--- src/main/kotlin/com/hana/jenkins/controller/HelloController.kt
+++ src/main/kotlin/com/hana/jenkins/controller/HelloController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 class HelloController {
     @GetMapping("/")
     fun hello(): String {
-        return "안녕<br>반가워 젠킨스!!<br>커밋해볼게!"
+        return "안녕<br>반가워 젠킨스!!<br>!!!!"
     }
     @GetMapping("/hi")
     fun hi(): String {
